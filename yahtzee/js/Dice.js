d98a0b8430f10b94ab10b19ff7a7bb57ce40d5ac
VinSjo <vincentsjogren@gmail.com>
2021-12-07 15:25:21 +0100

    testing some "dice-rolling" and split css into two files

--- yahtzee/js/Dice.js
+++ yahtzee/js/Dice.js
@@ -1,6 +1,6 @@
 import { setElementAttributes } from "./helpers.js";
 
-function diceIcon(value, size = 64) {
+function createDiceIcon(size = 64) {
 	const namespaceURI = "http://www.w3.org/2000/svg";
 
 	const svg = document.createElementNS(namespaceURI, "svg");
@@ -12,7 +12,6 @@ function diceIcon(value, size = 64) {
 
 	setElementAttributes(svg, {
 		class: "icon dice",
-		value: value,
 		width: size,
 		height: size,
 		viewBox: `0 0 ${size} ${size}`,
@@ -33,38 +32,80 @@ function diceIcon(value, size = 64) {
 
 	svg.append(rect);
 
-	const cPos = (x, y) => ({ cx: x, cy: y });
-	const dots = [],
-		min = rectRadius + dotRadius,
+	const min = rectRadius + dotRadius,
 		mid = size / 2,
 		max = size - min;
-	value % 2 !== 0 && dots.push(cPos(mid, mid));
-	value > 1 && dots.push(cPos(min, max), cPos(max, min));
-	value > 3 && dots.push(cPos(min, min), cPos(max, max));
-	value === 6 && dots.push(cPos(min, mid), cPos(max, mid));
-	svg.append(
-		...dots.map(position => setElementAttributes(dot.cloneNode(), position))
-	);
-	return svg;
+
+	const dots = [];
+
+	for (let i = 0; i < 7; i++) {
+		const clone = dot.cloneNode();
+		if (i !== 0) {
+			setElementAttributes(clone, {
+				cx: i % 2 !== 0 ? min : max,
+				cy: i === 1 || i === 4 ? max : i === 2 || i === 3 ? min : mid,
+			});
+		}
+		dots.push(clone);
+	}
+	svg.append(...dots);
+
+	return {
+		get element() {
+			return svg;
+		},
+		update(value) {
+			if (typeof value !== "number" || Number.isNaN(value)) return;
+			if (value < 1 || value > 6) {
+				svg.setAttribute("value", "0");
+				rect.classList.remove("show");
+				dots.forEach(dot => dot.classList.remove("show"));
+				return;
+			}
+			svg.setAttribute("value", `${value}`);
+			rect.classList.add("show");
+			const show = [
+				value % 2 !== 0,
+				value > 1,
+				value > 1,
+				value > 3,
+				value > 3,
+				value === 6,
+				value === 6,
+			];
+			dots.forEach((dot, index) => {
+				show[index]
+					? dot.classList.add("show")
+					: dot.classList.remove("show");
+			});
+		},
+	};
 }
 
 function createDice(diceCount = 5) {
-	return {
-		sides: 6,
+	const dice = {
 		count: diceCount,
 		_values: [],
 		_locked: [],
+		_icons: [],
 		get values() {
 			return this._values.concat(this._locked);
 		},
 		get icons() {
-			return this.values.map(value => diceIcon(value));
+			return this._icons.map(icon => icon.element);
 		},
 		roll() {
 			for (let i = 0; i < this.count - this._locked.length; i++) {
-				this._values[i] = Math.ceil(Math.random() * this.sides);
+				this._values[i] = Math.ceil(Math.random() * 6);
 			}
 			return this.values;
+		},
+		update() {
+			const values = this.values;
+			this._icons.forEach((icon, i) => {
+				const value = values[i] || 0;
+				icon.update(value);
+			});
 		},
 		reset() {
 			this._values = [];
@@ -75,6 +116,8 @@ function createDice(diceCount = 5) {
 			this._locked.push(this._values.splice(index, 1));
 		},
 	};
+	while (dice._icons.length < diceCount) dice._icons.push(createDiceIcon());
+	return dice;
 }
 
-export { createDice, diceIcon };
+export { createDice, createDiceIcon };
--- yahtzee/js/app.js
+++ yahtzee/js/app.js
@@ -1,10 +1,25 @@
 import { createDice } from "./dice.js";
+const $diceContainer = document.querySelector(".dice.container");
+const $rollButton = document.querySelector(".roll-button");
 
-const diceContainer = document.querySelector(".dice.container");
 const dice = createDice();
 
-// let intervalID = setInterval(() => {
-// 	diceContainer.innerHTML = null;
-// 	dice.roll();
-// 	diceContainer.append(...dice.icons);
-// }, 500);
+$diceContainer.append(...dice.icons);
+
+dice.roll();
+dice.update();
+
+let rollInterval = null;
+$rollButton.addEventListener("click", () => {
+	if (rollInterval !== null) return;
+	$rollButton.setAttribute("disabled", true);
+	rollInterval = setInterval(() => {
+		dice.roll();
+		dice.update();
+	}, 1000 / 10);
+	setTimeout(() => {
+		clearInterval(rollInterval);
+		rollInterval = null;
+		$rollButton.removeAttribute("disabled");
+	}, 1000);
+});
